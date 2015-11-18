4b3181aff7efea3cb9d19b518f07bbde6fe4ee57
AJ Friend <ajfriend@gmail.com>
2015-11-18 15:44:00 -0800

    remove openmp and 64bit blas options. can always add back in later

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ import sys
 from setup_helper import glober, add_blas_lapack_info
 import numpy
 
-# use 'export OMP_NUM_THREADS=16' to control num of threads (in that case, 16)
-USE_OPENMP = False
-
-# set to true if linking against blas/lapack libraries that use longs instead of ints for indices:
-USE_64_BIT_BLAS = False
-
 # ext is a dictionary collecting the keyword arguments that will be passed into 
 # the Extension module constructor
 # We first collect the arguments which are the same between the direct and
@@ -31,14 +25,6 @@ file_ext = '.pyx' if USE_CYTHON else '.c'
 if system() == 'Linux':
     ext['libraries'] += ['rt']
 
-if USE_OPENMP:
-    ext['define_macros'] += [('OPENMP', None)]
-    ext['extra_compile_args'] += ['-fopenmp']
-    ext['extra_link_args'] += ['-lgomp']
-
-if USE_64_BIT_BLAS:
-    ext['define_macros'] += [('BLAS64', None)]
-
 # location of SCS root directory, containing 'src/' etc.
 rootDir = 'c/scs/'
 
@@ -49,11 +35,9 @@ ext['define_macros'] += [('PYTHON', None), ('DLONG', None),
                          ('CTRLC', 1),     ('COPYAMATRIX', None)]
 ext['extra_compile_args'] += ["-O3"]
 
-
 # add the blas and lapack info
 add_blas_lapack_info(ext)
 ext['include_dirs'] += [numpy.get_include()]
-
 
 # create the extension module arguments for the direct solver version
 # deep copy so that the dictionaries do not point to the same list objects
