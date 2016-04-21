70762fbbb325d2a3c0dd3b06ff5ea99dcc2aec78
AJ Friend <ajfriend@gmail.com>
2016-04-21 15:05:41 -0700

    warm start mutability test

---
+++ src/scs/test/test_mutability.py
@@ -0,0 +1,33 @@
+import scs
+import pytest
+import scs.examples as ex
+import numpy as np
+
+
+def test_workspace():
+    data, cone = ex.simple_lp()
+    m,n = data['A'].shape
+    work = scs.Workspace(data, cone)
+
+    ws = dict(x=np.zeros(n), y=np.zeros(m), s=np.zeros(m))
+
+    sol = work.solve(warm_start = ws)
+
+    # make sure sol and ws contain *different* numpy arrays
+    assert any(ws['x'] != sol['x'])
+    assert any(ws['y'] != sol['y'])
+    assert any(ws['s'] != sol['s'])
+
+def test_solve():
+    data, cone = ex.simple_lp()
+    m,n = data['A'].shape
+    
+    ws = dict(x=np.zeros(n), y=np.zeros(m), s=np.zeros(m))
+
+    sol = scs.solve(data, cone, warm_start = ws)
+
+    # make sure sol and ws contain *different* numpy arrays
+    assert any(ws['x'] != sol['x'])
+    assert any(ws['y'] != sol['y'])
+    assert any(ws['s'] != sol['s'])
+
